9c8cb0f5db46f441d21631ad67b92c77ba4dde87
Sean Y <seanhyang1@gmail.com>
2025-10-02 08:23:21 -0400

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Header } from "@/components/header";
 import { DetailedFooter } from "@/components/DetailedFooter";
 import "./globals.css";
@@ -17,6 +18,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-sans antialiased bg-primary-background text-gray-900">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VGMRYS9DKN"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VGMRYS9DKN');
+          `}
+        </Script>
         <Header />
         <main className="min-h-screen px-6 py-8 pt-24">{children}</main>
         <DetailedFooter />
