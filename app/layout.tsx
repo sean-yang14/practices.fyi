a728df7c239cd2eed316141a08be67b6868a730c
Sean Y <seanhyang1@gmail.com>
2025-10-02 08:32:30 -0400

    switched to google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,19 +17,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="font-sans antialiased bg-primary-background text-gray-900">
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-VGMRYS9DKN"
-          strategy="afterInteractive"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-VGMRYS9DKN');
-          `}
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NMW6VTLS');`}
         </Script>
+      </head>
+      <body className="font-sans antialiased bg-primary-background text-gray-900">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NMW6VTLS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Header />
         <main className="min-h-screen px-6 py-8 pt-24">{children}</main>
         <DetailedFooter />
